6bf4ab1599919539a6691cd40299925b62aac728
Ernest Offiong <ernest.offiong@gmail.com>
2019-05-03 09:49:24 +0100

    chore: debugging expo push notification

--- models/utils/expoPushNotification.js
+++ models/utils/expoPushNotification.js
@@ -19,7 +19,7 @@ module.exports = async (text, title, pushTokens) => {
   }
 
   let chunks = await expo.chunkPushNotifications(messages);
-
+  console.log(chunks)
   let tickets = [];
   (async () => {
     for (let chunk of chunks) {
@@ -34,34 +34,34 @@ module.exports = async (text, title, pushTokens) => {
     }
   })();
 
-  let receiptIds = [];
-  for (let ticket of tickets) {
-    // NOTE: Not all tickets have IDs; for example, tickets for notifications
-    // that could not be enqueued will have error information and no receipt ID.
-    if (ticket.id) {
-      receiptIds.push(ticket.id);
-    }
-  }
+  // let receiptIds = [];
+  // for (let ticket of tickets) {
+  //   // NOTE: Not all tickets have IDs; for example, tickets for notifications
+  //   // that could not be enqueued will have error information and no receipt ID.
+  //   if (ticket.id) {
+  //     receiptIds.push(ticket.id);
+  //   }
+  // }
 
-  let receiptIdChunks = await expo.chunkPushNotificationReceiptIds(receiptIds);
-  (async () => {
-    for (let chunk of receiptIdChunks) {
-      try {
-        let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
-        console.log(receipts);
-        for (let receipt of receipts) {
-          if (receipt.status === 'ok') {
-            continue;
-          } else if (receipt.status === 'error') {
-            console.error(`There was an error sending a notification: ${receipt.message}`);
-            if (receipt.details && receipt.details.error) {
-              console.error(`The error code is ${receipt.details.error}`);
-            }
-          }
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  })();
+  // let receiptIdChunks = await expo.chunkPushNotificationReceiptIds(receiptIds);
+  // (async () => {
+  //   for (let chunk of receiptIdChunks) {
+  //     try {
+  //       let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+  //       console.log(receipts);
+  //       for (let receipt of receipts) {
+  //         if (receipt.status === 'ok') {
+  //           continue;
+  //         } else if (receipt.status === 'error') {
+  //           console.error(`There was an error sending a notification: ${receipt.message}`);
+  //           if (receipt.details && receipt.details.error) {
+  //             console.error(`The error code is ${receipt.details.error}`);
+  //           }
+  //         }
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  // })();
 }
